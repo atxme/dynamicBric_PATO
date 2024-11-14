58a600f6d5f11f9a874dfd78a5cbb9028c1ca10f
atxmee <christophe.benedetti@gmail.com>
2024-11-14 13:09:05 +0100

    Ajoutez des fichiers projet.

---
+++ dynamicBric_PATO.cpp
@@ -0,0 +1,12 @@
+﻿// dynamicBric_PATO.cpp : définit le point d'entrée de l'application.
+//
+
+#include "dynamicBric_PATO.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
