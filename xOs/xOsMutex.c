9d371ad22794dac45ae4ba0bf6836a3ad3c8dd54
Christophe benedetti <christophe.benedetti15@gmail.com>
2025-01-10 15:17:57 +0100

    update add mutex

---
+++ xOs/xOsMutex.c
@@ -0,0 +1,190 @@
+////////////////////////////////////////////////////////////
+//  mutex source file
+//  implements mutex functions
+//
+// general discloser: copy or share the file is forbidden
+// Written : 14/11/2024
+////////////////////////////////////////////////////////////
+
+#include "xOs/xOsMutex.h"
+#include "assert/xAssert.h"
+#include <string.h>
+
+////////////////////////////////////////////////////////////
+/// mutexCreate
+////////////////////////////////////////////////////////////
+int mutexCreate(xos_mutex_t* p_ptMutex)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+#ifdef _WIN32
+    p_ptMutex->t_mutex = CreateMutex(NULL, FALSE, NULL);
+    if (p_ptMutex->t_mutex == NULL) {
+        return MUTEX_ERROR;
+    }
+#else
+    pthread_mutexattr_t l_tAttr;
+    pthread_mutexattr_init(&l_tAttr);
+    pthread_mutexattr_settype(&l_tAttr, PTHREAD_MUTEX_RECURSIVE);
+
+    if (pthread_mutex_init(&p_ptMutex->t_mutex, &l_tAttr) != 0) {
+        pthread_mutexattr_destroy(&l_tAttr);
+        return MUTEX_ERROR;
+    }
+    pthread_mutexattr_destroy(&l_tAttr);
+#endif
+
+    p_ptMutex->t_iState = MUTEX_UNLOCKED;
+    p_ptMutex->t_ulTimeout = MUTEX_DEFAULT_TIMEOUT;
+
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexLock
+////////////////////////////////////////////////////////////
+int mutexLock(xos_mutex_t* p_ptMutex)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+#ifdef _WIN32
+    DWORD l_dwResult = WaitForSingleObject(p_ptMutex->t_mutex, INFINITE);
+    if (l_dwResult == WAIT_FAILED) {
+        return MUTEX_ERROR;
+    }
+#else
+    if (pthread_mutex_lock(&p_ptMutex->t_mutex) != 0) {
+        return MUTEX_ERROR;
+    }
+#endif
+
+    p_ptMutex->t_iState = MUTEX_LOCKED;
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexTryLock
+////////////////////////////////////////////////////////////
+int mutexTryLock(xos_mutex_t* p_ptMutex)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+#ifdef _WIN32
+    DWORD l_dwResult = WaitForSingleObject(p_ptMutex->t_mutex, 0);
+    if (l_dwResult == WAIT_TIMEOUT) {
+        return MUTEX_TIMEOUT;
+    }
+    if (l_dwResult == WAIT_FAILED) {
+        return MUTEX_ERROR;
+    }
+#else
+    int l_iResult = pthread_mutex_trylock(&p_ptMutex->t_mutex);
+    if (l_iResult == EBUSY) {
+        return MUTEX_TIMEOUT;
+    }
+    if (l_iResult != 0) {
+        return MUTEX_ERROR;
+    }
+#endif
+
+    p_ptMutex->t_iState = MUTEX_LOCKED;
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexLockTimeout
+////////////////////////////////////////////////////////////
+int mutexLockTimeout(xos_mutex_t* p_ptMutex, unsigned long p_ulTimeout)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+#ifdef _WIN32
+    DWORD l_dwResult = WaitForSingleObject(p_ptMutex->t_mutex, p_ulTimeout);
+    if (l_dwResult == WAIT_TIMEOUT) {
+        return MUTEX_TIMEOUT;
+    }
+    if (l_dwResult == WAIT_FAILED) {
+        return MUTEX_ERROR;
+    }
+#else
+    struct timespec l_tTimeout;
+    clock_gettime(CLOCK_REALTIME, &l_tTimeout);
+    l_tTimeout.tv_sec += p_ulTimeout / 1000;
+    l_tTimeout.tv_nsec += (p_ulTimeout % 1000) * 1000000;
+
+    int l_iResult = pthread_mutex_timedlock(&p_ptMutex->t_mutex, &l_tTimeout);
+    if (l_iResult == ETIMEDOUT) {
+        return MUTEX_TIMEOUT;
+    }
+    if (l_iResult != 0) {
+        return MUTEX_ERROR;
+    }
+#endif
+
+    p_ptMutex->t_iState = MUTEX_LOCKED;
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexUnlock
+////////////////////////////////////////////////////////////
+int mutexUnlock(xos_mutex_t* p_ptMutex)
+{
+    X_ASSERT(p_ptMutex != NULL);
+    X_ASSERT(p_ptMutex->t_iState == MUTEX_LOCKED);
+
+#ifdef _WIN32
+    if (!ReleaseMutex(p_ptMutex->t_mutex)) {
+        return MUTEX_ERROR;
+    }
+#else
+    if (pthread_mutex_unlock(&p_ptMutex->t_mutex) != 0) {
+        return MUTEX_ERROR;
+    }
+#endif
+
+    p_ptMutex->t_iState = MUTEX_UNLOCKED;
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexDestroy
+////////////////////////////////////////////////////////////
+int mutexDestroy(xos_mutex_t* p_ptMutex)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+#ifdef _WIN32
+    if (!CloseHandle(p_ptMutex->t_mutex)) {
+        return MUTEX_ERROR;
+    }
+#else
+    if (pthread_mutex_destroy(&p_ptMutex->t_mutex) != 0) {
+        return MUTEX_ERROR;
+    }
+#endif
+
+    p_ptMutex->t_iState = MUTEX_UNLOCKED;
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexSetTimeout
+////////////////////////////////////////////////////////////
+int mutexSetTimeout(xos_mutex_t* p_ptMutex, unsigned long p_ulTimeout)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+    p_ptMutex->t_ulTimeout = p_ulTimeout;
+    return MUTEX_OK;
+}
+
+////////////////////////////////////////////////////////////
+/// mutexGetState
+////////////////////////////////////////////////////////////
+int mutexGetState(xos_mutex_t* p_ptMutex)
+{
+    X_ASSERT(p_ptMutex != NULL);
+
+    return p_ptMutex->t_iState;
+}
--- xOs/xOsMutex.h
+++ xOs/xOsMutex.h
@@ -1,12 +1,113 @@
 ////////////////////////////////////////////////////////////
-//  os critical header file
-//  defines the os function for critical section manipulation
+//  mutex header file
+//  defines the mutex types and constants
 //
 // general discloser: copy or share the file is forbidden
 // Written : 14/11/2024
 ////////////////////////////////////////////////////////////
+#pragma once
 
-#include <mutex.h>
+#ifndef XOS_MUTEX_H_
+#define XOS_MUTEX_H_
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <pthread.h>
+#include <errno.h>
+#include <time.h>
+#endif
+
+// Mutex error codes
+#define MUTEX_OK              0
+#define MUTEX_ERROR         -1
+#define MUTEX_TIMEOUT      -2
+#define MUTEX_INVALID      -3
+#define MUTEX_ALREADY_INIT -4
+#define MUTEX_NOT_INIT     -5
+
+// Mutex states
+#define MUTEX_UNLOCKED 0
+#define MUTEX_LOCKED   1
+
+// Default timeout value (ms)
+#define MUTEX_DEFAULT_TIMEOUT 1000
+
+//////////////////////////////////
+/// @brief mutex structure
+/// @param t_mutex : mutex handle
+/// @param t_iState : mutex state
+/// @param t_ulTimeout : timeout value in milliseconds
+//////////////////////////////////
+typedef struct {
+#ifdef _WIN32
+    HANDLE t_mutex;
+#else
+    pthread_mutex_t t_mutex;
+#endif
+    int t_iState;
+    unsigned long t_ulTimeout;
+} xos_mutex_t;
+
+//////////////////////////////////
+/// @brief create mutex
+/// @param p_ptMutex : mutex structure pointer
+/// @return : success or error code
+//////////////////////////////////
+int mutexCreate(xos_mutex_t* p_ptMutex);
+
+//////////////////////////////////
+/// @brief lock mutex
+/// @param p_ptMutex : mutex structure pointer
+/// @return : success or error code
+//////////////////////////////////
+int mutexLock(xos_mutex_t* p_ptMutex);
+
+//////////////////////////////////
+/// @brief try to lock mutex
+/// @param p_ptMutex : mutex structure pointer
+/// @return : success or error code
+//////////////////////////////////
+int mutexTryLock(xos_mutex_t* p_ptMutex);
+
+//////////////////////////////////
+/// @brief lock mutex with timeout
+/// @param p_ptMutex : mutex structure pointer
+/// @param p_ulTimeout : timeout value in milliseconds
+/// @return : success or error code
+//////////////////////////////////
+int mutexLockTimeout(xos_mutex_t* p_ptMutex, unsigned long p_ulTimeout);
+
+//////////////////////////////////
+/// @brief unlock mutex
+/// @param p_ptMutex : mutex structure pointer
+/// @return : success or error code
+//////////////////////////////////
+int mutexUnlock(xos_mutex_t* p_ptMutex);
+
+//////////////////////////////////
+/// @brief destroy mutex
+/// @param p_ptMutex : mutex structure pointer
+/// @return : success or error code
+//////////////////////////////////
+int mutexDestroy(xos_mutex_t* p_ptMutex);
+
+//////////////////////////////////
+/// @brief set mutex timeout
+/// @param p_ptMutex : mutex structure pointer
+/// @param p_ulTimeout : timeout value in milliseconds
+/// @return : success or error code
+//////////////////////////////////
+int mutexSetTimeout(xos_mutex_t* p_ptMutex, unsigned long p_ulTimeout);
+
+//////////////////////////////////
+/// @brief get mutex state
+/// @param p_ptMutex : mutex structure pointer
+/// @return : mutex state or error code
+//////////////////////////////////
+int mutexGetState(xos_mutex_t* p_ptMutex);
+
+#endif // XOS_MUTEX_H_
 
 
 
